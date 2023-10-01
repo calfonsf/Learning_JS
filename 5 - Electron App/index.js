2af352e97089660a4622a032a36a7b8378046cb7
unknown <calfonsf@gmail.com>
2023-10-01 15:26:04 -0400

    update

--- 5 - Electron App/index.js
+++ 5 - Electron App/index.js
@@ -4,11 +4,13 @@ const url = require('url');
 function boot() {
 
     win = new BrowserWindow()
-    win.loadURL(url.format({
+    /* win.loadURL(url.format({
         pathname: 'index.html',
         slashes: true
 
-    }))
+    })) */
+
+    win.loadURL('./index.html')
 
 }
 
