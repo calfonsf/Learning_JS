c7a760ea431655e7f0185f9e3843fcd96263aaf5
calfonsf <calfonsf@gmail.com>
2023-12-24 16:28:56 -0500

    Added Svelte and Vue(CDN) examples

---
+++ 8 - Frameworks/SvelteJs/src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+    let tasks = [];
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newtask = event.target.newTask.value;
+
+        if (newtask) {
+            tasks.push(newtask)
+            tasks = tasks
+        } else {
+            console.log("Empty Task");
+        }
+    };
+</script>
+
+<main>
+    <h1>TODO List</h1>
+    <form on:submit={handleSubmit}>
+        <input type="text" placeholder="New Task" name="newTask" />
+        <input type="submit" />
+    </form>
+
+    <h2>Tasks List</h2>
+    <ul>
+        {#each tasks as task}
+            <li>{task}</li>
+        {/each}
+    </ul>
+</main>
