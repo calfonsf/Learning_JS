a096b43d5a1266ba752379966abeaaa7b636a62b
calfonsf <calfonsf@gmail.com>
2023-12-26 11:35:11 -0500

    Migrate Vue example from CDN to Vite

---
+++ 8 - Frameworks/VueJs/src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+</script>
+
+<template>
+    <body>
+        <div id="app">
+            <h1>TODO List</h1>
+            <form @submit.prevent="handleSubmit">
+                <input type="text" placeholder="New Task" v-model="newTask" />
+                <input type="submit" />
+            </form>
+            <h2>Tasks List</h2>
+            <ul>
+                <li v-for="task in taskList">{{ task }}</li>
+            </ul>
+        </div>
+    </body>
+</template>
+
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+            taskList: [],
+            newTask: "",
+        };
+    },
+    methods: {
+        renderList() {
+            map = this.taskList.map((element) => {
+                return element;
+            });
+        },
+        handleSubmit() {
+            if (this.newTask) {
+                this.taskList.push(this.newTask);
+            } else {
+                console.log("Empty Task");
+            }
+        },
+    },
+};
+</script>
